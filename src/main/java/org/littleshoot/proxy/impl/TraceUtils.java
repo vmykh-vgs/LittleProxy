dce61f34a257d17394ac8476721a557bfef72f7d
Zinovii Dmytriv <zinovii.dmytriv@verygood.systems>
2021-06-29 15:27:53 +0300

    Handle transparency for Forward Proxy (#86)
    Handle transparency for Forward Proxy

---
+++ src/main/java/org/littleshoot/proxy/impl/TraceUtils.java
@@ -0,0 +1,10 @@
+package org.littleshoot.proxy.impl;
+
+public final class TraceUtils {
+
+  public static final String HEADER_X_B3_TRACEID = "X-B3-TraceId";
+  public static final String HEADER_X_B3_SPANID = "X-B3-SpanId";
+  public static final String HEADER_X_B3_SAMPLED = "X-B3-Sampled";
+  public static final String HEADER_X_B3_PARENTSPANID = "X-B3-ParentSpanId";
+  public static final String HEADER_VGS_REQUEST_ID = "VGS-Request-Id";
+}
---
+++ src/main/java/org/littleshoot/proxy/impl/TracingAwareHttpRequestEncoder.java
@@ -0,0 +1,38 @@
+package org.littleshoot.proxy.impl;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpRequestEncoder;
+import io.netty.util.AttributeKey;
+
+public class TracingAwareHttpRequestEncoder extends HttpRequestEncoder {
+
+  private ChannelHandlerContext ctx;
+
+  public static final AttributeKey<String> IS_FORWARD_PROXY_TRANSPARENT_ATTRIBUTE = AttributeKey
+      .valueOf("isForwardProxyTransparent");
+
+  @Override
+  public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+    this.ctx = ctx;
+  }
+
+  @Override
+  protected void sanitizeHeadersBeforeEncode(HttpRequest msg, boolean isAlwaysEmpty) {
+    HttpHeaders headers = msg.headers();
+
+    if (isForwardProxyTransparent()) {
+      headers.remove(TraceUtils.HEADER_X_B3_TRACEID);
+      headers.remove(TraceUtils.HEADER_X_B3_SPANID);
+      headers.remove(TraceUtils.HEADER_X_B3_SAMPLED);
+      headers.remove(TraceUtils.HEADER_X_B3_PARENTSPANID);
+      headers.remove(TraceUtils.HEADER_VGS_REQUEST_ID);
+    }
+  }
+
+  private boolean isForwardProxyTransparent() {
+    String isTransparent = ctx.channel().attr(IS_FORWARD_PROXY_TRANSPARENT_ATTRIBUTE).get();
+    return "true".equalsIgnoreCase(isTransparent);
+  }
+}
